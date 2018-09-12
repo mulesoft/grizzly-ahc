f185cc7fdeeb5ee573f4110b7deffa0cb5876977
fabian.gonzalez <fabian.gonzalez@mulesoft.com>
2018-09-12 11:00:26 -0300

    MULE-15667: Grizzly should reset requestInput stream in NTLM type3 message. #7

--- src/main/java/com/ning/http/client/generators/InputStreamBodyGenerator.java
+++ src/main/java/com/ning/http/client/generators/InputStreamBodyGenerator.java
@@ -48,10 +48,17 @@ public class InputStreamBodyGenerator implements BodyGenerator {
 
     @Override
     public Body createBody() throws IOException {
-        if (inputStream.markSupported()) {
-          inputStream.mark(0);
+      if (inputStream.markSupported()) {
+        try {
           inputStream.reset();
+        } catch (IOException ioe) {
+          if (logger.isDebugEnabled()) {
+            logger.debug("Unable to reset the input stream: %s", ioe.getMessage());
+          }
         }
+
+        inputStream.mark(0);
+      }
         return new ISBody();
     }
 
