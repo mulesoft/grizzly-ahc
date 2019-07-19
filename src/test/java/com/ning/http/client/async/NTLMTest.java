b720a4bd861df04b2c879b206140d88386949bba
Pablo Balbi <pablo.l.balbi@gmail.com>
2019-07-19 17:34:48 -0300

    NTLM custom connection managment test

--- src/test/java/com/ning/http/client/async/NTLMTest.java
+++ src/test/java/com/ning/http/client/async/NTLMTest.java
@@ -13,52 +13,53 @@
  */
 package com.ning.http.client.async;
 
-import org.apache.commons.io.IOUtils;
-import org.eclipse.jetty.http.HttpMethod;
-import org.eclipse.jetty.server.handler.AbstractHandler;
-import org.eclipse.jetty.server.handler.HandlerWrapper;
-import org.testng.Assert;
-import org.testng.annotations.Test;
+import static org.apache.commons.io.IOUtils.toByteArray;
+import static org.eclipse.jetty.http.HttpMethod.GET;
+import static org.eclipse.jetty.http.HttpMethod.POST;
+import static org.slf4j.LoggerFactory.getLogger;
 
 import com.ning.http.client.AsyncHttpClient;
 import com.ning.http.client.AsyncHttpClientConfig;
 import com.ning.http.client.Realm;
 import com.ning.http.client.Realm.AuthScheme;
 import com.ning.http.client.Realm.RealmBuilder;
-import com.ning.http.client.generators.InputStreamBodyGenerator;
 import com.ning.http.client.Request;
 import com.ning.http.client.RequestBuilder;
 import com.ning.http.client.Response;
+import com.ning.http.client.generators.InputStreamBodyGenerator;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import static org.apache.commons.io.IOUtils.toByteArray;
-import static org.eclipse.jetty.http.HttpMethod.GET;
-import static org.eclipse.jetty.http.HttpMethod.POST;
-
-import java.io.BufferedInputStream;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.util.Random;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.eclipse.jetty.server.handler.HandlerWrapper;
+import org.slf4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public abstract class NTLMTest extends AbstractBasicTest {
 
     private static final String PAYLOAD = "PAYLOAD";
-    
+    private AsyncHttpClient client;
+    private static final Logger LOGGER = getLogger(NTLMTest.class);
+    private static final Set<String> seenClients = new HashSet<>();
+
     public static class NTLMHandler extends HandlerWrapper {
 
         @Override
         public void handle(String pathInContext, org.eclipse.jetty.server.Request request, HttpServletRequest httpRequest,
                 HttpServletResponse httpResponse) throws IOException, ServletException {
+
+            seenClients.add(request.getRemoteInetSocketAddress().toString());
 
             String authorization = httpRequest.getHeader("Authorization");
             if (authorization == null) {
@@ -82,6 +83,11 @@ public abstract class NTLMTest extends AbstractBasicTest {
             httpResponse.getOutputStream().flush();
             httpResponse.getOutputStream().close();
         }
+    }
+
+    @BeforeTest
+    public void cleanTestWatchers() {
+        seenClients.clear();
     }
 
     @Override
@@ -143,5 +149,36 @@ public abstract class NTLMTest extends AbstractBasicTest {
     @Test
     public void preemptiveNTLMAuthGetTest() throws IOException, InterruptedException, ExecutionException {
       ntlmAuthWithGetTest(realmBuilderBase().setUsePreemptiveAuth(true));
+    }
+
+    @Test
+    public void ntlmSubsequentAuthAgainstRemoteIIS() throws Exception {
+
+        // Build client to be used
+        AsyncHttpClientConfig config = new AsyncHttpClientConfig.Builder().build();
+        client = getAsyncHttpClient(config);
+
+        // Make good request
+        Future<Response> responseFuture =  makeNtlmAuthenticatedRequestWithCredentials(realmBuilderBase());
+        LOGGER.error("Making good request");
+        Assert.assertEquals(responseFuture.get().getStatusCode(), 200);
+
+        // Make bad request
+        responseFuture =  makeNtlmAuthenticatedRequestWithCredentials(realmBuilderBase().setPassword("perro"));
+        LOGGER.error("Making bad request");
+        Assert.assertEquals(responseFuture.get().getStatusCode(), 401);
+        client.close();
+
+        Assert.assertEquals(seenClients.size(), 2);
+    }
+
+    private Future<Response> makeNtlmAuthenticatedRequestWithCredentials(RealmBuilder realmBuilder) throws InterruptedException, ExecutionException, IOException {
+        Request request = new RequestBuilder(GET.asString())
+                .setUrl(getTargetUrl())
+                // .setUrl("http://10.250.1.231:5959/")
+                .setRealm(realmBuilder.build())
+                .build();
+
+        return client.executeRequest(request);
     }
 }
