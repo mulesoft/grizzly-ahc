b388e4ca59b2a3a47643433a5d9edfd51ee21a22
Ayelen Lopez Bizon <ayelen.lopez@mulesoft.com>
2022-06-14 11:33:34 -0300

    Fix the import issue and add licenses.

--- src/test/java/com/ning/http/util/AuthenticatorUtilsTest.java
+++ src/test/java/com/ning/http/util/AuthenticatorUtilsTest.java
@@ -1,3 +1,15 @@
+/*
+ * Copyright (c) 2010-2012 Sonatype, Inc. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
 package com.ning.http.util;
 
 import static com.ning.http.client.async.RetryNonBlockingIssue.findFreePort;
@@ -5,7 +17,7 @@ import static com.ning.http.client.async.RetryNonBlockingIssue.findFreePort;
 import static java.lang.Boolean.FALSE;
 import static java.lang.Boolean.TRUE;
 
-import static org.junit.Assert.assertEquals;
+import static org.testng.Assert.assertEquals;
 
 import com.ning.http.client.ProxyServer;
 import com.ning.http.client.Realm.AuthScheme;
