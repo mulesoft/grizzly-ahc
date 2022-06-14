2dbc7c2bb6dd85d161694db27e12096f160317a8
Ayelen Lopez Bizon <ayelen.lopez@mulesoft.com>
2022-06-14 11:05:46 -0300

    Use the existing test framework.

--- src/test/java/com/ning/http/util/AuthenticatorUtilsTest.java
+++ src/test/java/com/ning/http/util/AuthenticatorUtilsTest.java
@@ -13,17 +13,13 @@ import com.ning.http.client.Request;
 import com.ning.http.client.RequestBuilder;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collection;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
-@RunWith(Parameterized.class)
 public class AuthenticatorUtilsTest {
 
-    private final int port;
+    private final int port = findFreePort();
     private static final String METHOD = "GET";
     private static final String URL = "http://127.0.0.1:%d/foo/test";
     private static final String HOST = "127.0.0.1";
@@ -35,37 +31,12 @@ public class AuthenticatorUtilsTest {
     private static final String NTLM_HEADER_VALUE =
         "NTLM TlRMTVNTUAADAAAAGAAYAEgAAAAYABgAYAAAABQAFAB4AAAADAAMAIwAAAASABIAmAAAAAAAAACqAAAAAYIAAgUBKAoAAAAPrYfKbe/jRoW5xDxHeoxC1gBmfWiS5+iX4OAN4xBKG/IFPwfH3agtPEia6YnhsADTVQBSAFMAQQAtAE0ASQBOAE8AUgBaAGEAcABoAG8AZABMAGkAZwBoAHQAQwBpAHQAeQA=";
 
-    private final boolean connect;
-    private final AuthScheme authScheme;
-    private final String principal;
-    private final String ntlDomain;
-    private final String headerKey;
-    private final String headerValue;
-    private final String proxyAuthorizationExpected;
-    private final boolean properProxyAuthorization;
-
-    public AuthenticatorUtilsTest(boolean connect,
-                                  AuthScheme authScheme,
-                                  String principal,
-                                  String ntlDomain,
-                                  String headerKey,
-                                  String headerValue,
-                                  String proxyAuthorization,
-                                  boolean properProxyAuthorization) throws IOException {
-        this.port = findFreePort();
-        this.connect = connect;
-        this.authScheme = authScheme;
-        this.principal = principal;
-        this.ntlDomain = ntlDomain;
-        this.headerKey = headerKey;
-        this.headerValue = headerValue;
-        this.proxyAuthorizationExpected = proxyAuthorization;
-        this.properProxyAuthorization = properProxyAuthorization;
+    public AuthenticatorUtilsTest() throws IOException {
     }
 
-    @Parameterized.Parameters
-    public static Collection<?> parameters() {
-        return Arrays.asList(new Object[][] {
+    @DataProvider(name = "proxyAuthorization")
+    public Object[][] createData() {
+        return new Object[][] {
             { TRUE, AuthScheme.BASIC, null, null, null, null, null, TRUE },
             { TRUE, AuthScheme.BASIC, null, null, null, null, NTLM_MSG_TYPE_1, FALSE },
             { TRUE, AuthScheme.DIGEST, null, null, null, null, null, TRUE },
@@ -84,11 +55,14 @@ public class AuthenticatorUtilsTest {
             { FALSE, AuthScheme.NTLM, NTLM_PRINCIPAL, NTLM_DOMAIN, NTLM_HEADER_KEY, NTLM_HEADER_VALUE, null, FALSE },
             { TRUE, AuthScheme.SPNEGO, null, null, null, null, null, TRUE },
             { TRUE, AuthScheme.SPNEGO, null, null, null, null, NTLM_MSG_TYPE_1, FALSE }
-        });
+        };
     }
 
-    @Test
-    public void perConnectionProxyAuthorizationHeaderReturnsTheProxyAuthorizationExpected() throws IOException {
+    @Test(dataProvider = "proxyAuthorization")
+    public void perConnectionProxyAuthorizationHeaderReturnsTheProxyAuthorizationExpected(
+        boolean connect, AuthScheme authScheme, String principal, String ntlDomain, String headerKey,
+        String headerValue, String proxyAuthorizationExpected, boolean properProxyAuthorization) throws IOException {
+
         final ProxyServer proxyServer = new ProxyServer(HOST, port, principal, PASSWORD);
         proxyServer.setNtlmDomain(ntlDomain);
         proxyServer.setScheme(authScheme);
